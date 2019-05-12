d83a15bd931581d579373022e08ea548a7921e9c
Ghabriel <ghabriel.nunes@gmail.com>
2019-05-12 15:11:50 -0300

    DifficultyAttributes code cleanup

--- src/components/DifficultyAttributes.tsx
+++ src/components/DifficultyAttributes.tsx
@@ -8,52 +8,35 @@ export interface DifficultyAttributesProps {
     beatmap: Beatmap;
 }
 
+function trait(key: string, value: number, decimalPlaces?: number): JSX.Element {
+    return (
+        <div style={styles.trait}>
+            <div style={styles.traitKey}>
+                {key}
+            </div>
+
+            <PartialBar fraction={value / 10} />
+
+            <div style={styles.traitValue}>
+                {decimalPlaces === undefined ? value : round(value, decimalPlaces)}
+            </div>
+        </div>
+    );
+}
+
 export const DifficultyAttributes: React.FunctionComponent<DifficultyAttributesProps> = ({ beatmap }) => {
     return (
         <div style={styles.frame}>
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>HP Drain</div>
-                <PartialBar fraction={beatmap.hpDrainRate / 10} />
-                <div style={styles.traitValue}>{beatmap.hpDrainRate}</div>
-            </div>
-
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Circle Size</div>
-                <PartialBar fraction={beatmap.circleSize / 10} />
-                <div style={styles.traitValue}>{beatmap.circleSize}</div>
-            </div>
-
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Overall Difficulty</div>
-                <PartialBar fraction={beatmap.overallDifficulty / 10} />
-                <div style={styles.traitValue}>{beatmap.overallDifficulty}</div>
-            </div>
-
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Approach Rate</div>
-                <PartialBar fraction={beatmap.approachRate / 10} />
-                <div style={styles.traitValue}>{beatmap.approachRate}</div>
-            </div>
+            {trait('HP Drain', beatmap.hpDrainRate)}
+            {trait('Circle Size', beatmap.circleSize)}
+            {trait('Overall Difficulty', beatmap.overallDifficulty)}
+            {trait('Approach Rate', beatmap.approachRate)}
 
             <hr style={styles.traitDivisor} />
 
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Aim Strain</div>
-                <PartialBar fraction={beatmap.aimStrain / 10} />
-                <div style={styles.traitValue}>{round(beatmap.aimStrain, 3)}</div>
-            </div>
-
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Speed Strain</div>
-                <PartialBar fraction={beatmap.speedStrain / 10} />
-                <div style={styles.traitValue}>{round(beatmap.speedStrain, 3)}</div>
-            </div>
-
-            <div style={styles.trait}>
-                <div style={styles.traitKey}>Star Rating</div>
-                <PartialBar fraction={beatmap.starRating / 10} />
-                <div style={styles.traitValue}>{round(beatmap.starRating, 3)}</div>
-            </div>
+            {trait('Aim Strain', beatmap.aimStrain, 3)}
+            {trait('Speed Strain', beatmap.speedStrain, 3)}
+            {trait('Star Rating', beatmap.starRating, 3)}
         </div>
     );
 };
