0cb55cbca99d4d8c26b6045ca732155b38565eba
Ghabriel <ghabriel.nunes@gmail.com>
2019-05-18 16:40:24 -0300

    Added some tests for FractionalBar

---
+++ src/components/__tests__/FractionalBar.spec.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { PartialBar } from '../base/PartialBar';
+import { FractionalStat, FractionalStatProps } from '../base/stats/FractionalStat';
+import { Stat } from '../base/stats/Stat';
+
+const render = (props: FractionalStatProps) => {
+    const component = create(<FractionalStat {...props} />);
+    const testInstance = component.root;
+
+    return testInstance;
+};
+
+describe('FractionalBar component', () => {
+    it('should display the correct label', () => {
+        // given
+        const props: FractionalStatProps = {
+            fraction: 0,
+            label: 'test label',
+        };
+
+        // when
+        const testInstance = render(props);
+        const getStat = () => testInstance.findByType(Stat);
+
+        // then
+        expect(getStat).not.toThrow();
+        expect(getStat().props.label).toBe('test label');
+    });
+
+    it('should contain a PartialBar with the correct value', () => {
+        // given
+        const props: FractionalStatProps = {
+            fraction: 0.6,
+            label: '',
+        };
+
+        // when
+        const testInstance = render(props);
+        const getPartialBar = () => testInstance.findByType(PartialBar);
+
+        // then
+        expect(getPartialBar).not.toThrow();
+        expect(getPartialBar().props.fraction).toBe(0.6);
+    });
+
+    it('should display its children', () => {
+        // given
+        const props: React.PropsWithChildren<FractionalStatProps> = {
+            fraction: 0,
+            label: '',
+            children: <span id='test-node'></span>
+        };
+
+        // when
+        const testInstance = render(props);
+        const getTestNode = () => testInstance.findByProps({ id: 'test-node' });
+
+        // then
+        expect(getTestNode).not.toThrow();
+    });
+});
