a5efd8b9126cfb4fc39184c61dcbcd62f55aee7e
Ghabriel <ghabriel.nunes@gmail.com>
2019-06-28 19:47:47 -0300

    Stat styles moved to Main.scss

--- src/components/base/stats/Stat.tsx
+++ src/components/base/stats/Stat.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { createStyleSheet } from '../../../helpers/utilities';
 
 export interface StatProps {
     label: string;
 }
 
-const styles = createStyleSheet({
-    body: {
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'row',
-    },
-
-    label: {
-        fontWeight: 'bold',
-        width: '150px',
-    },
-});
-
 export const Stat: React.FC<StatProps> = ({ label, children }) => {
     return (
-        <div style={styles.body}>
-            <div style={styles.label} id='label-container'>
+        <div className="stat">
+            <div className="label" id="label-container">
                 {label}
             </div>
 
